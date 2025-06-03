140fa57e1665a66bdbb9062122a5a6f50de6d955
Gabriel Ribeiro <bieelrr4@gmail.com>
2025-06-03 20:27:36 -0300

    feat: starting petdev project

---
+++ src/app/_components/hero.tsx
@@ -0,0 +1,53 @@
+import { WhatsappLogo } from '@phosphor-icons/react/dist/ssr'
+
+export function Hero() {
+  return (
+    <section className="bg-[#E84C3D] text-white relative overflow-hidden">
+
+
+      <div className="container mx-auto pt-16 pb-16 md:pb-0 px-4 relative">
+
+        <article className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+
+          <div className="space-y-6">
+            <h1
+              className="text-3xl md:text-4xl lg:text-5xl font-bold leading-10"
+              data-aos="fade-down"
+            >
+              Seu pet merece cuidado, carinho e atenção especial.
+            </h1>
+            <p className=" lg:text-lg" data-aos="fade-right">
+              Oferecemos os melhores serviços para garantir o bem-estar e a felicidade do seu amigo de quatro patas.
+            </p>
+
+
+            <a
+              data-aos="fade-up"
+              data-aos-delay="500"
+              target='_blank'
+              href={`https://wa.me/556799998800?text=Olá vim pelo site e gostaria de mais informações`}
+              className="bg-green-500 px-5 py-2 rounded-md font-semibold flex items-center justify-center w-fit gap-2"
+            >
+              <WhatsappLogo className='w-5 h-5' />
+              Contato via WhatsApp
+            </a>
+
+            <div className="mt-8">
+              <p className="text-sm mb-4">
+                <b className="bg-black text-white px-2 py-1 rounded-md">5%</b> de desconto na primeira compra.
+              </p>
+            </div>
+
+          </div>
+
+          <div className="hidden md:block h-full relative">
+          </div>
+
+        </article>
+
+      </div>
+
+
+    </section>
+  )
+}
